967644f05d90ae0060807af978adb8d66167848f
Ian <i.r.butterworth@gmail.com>
2020-05-19 18:00:04 -0400

    add start of stream load methods

--- src/io.jl
+++ src/io.jl
@@ -22,6 +22,39 @@ function load(fpath::String; gamma::Union{Nothing,Float64}=nothing, expand_palet
     png_ptr = create_read_struct()
     info_ptr = create_info_struct(png_ptr)
     png_init_io(png_ptr, fp)
+    buffer = _load(png_ptr, info_ptr)
+    close_png(fp)
+    return formatbuffer(buffer)
+end
+function load(s::IO; gamma::Union{Nothing,Float64}=nothing, expand_paletted::Bool=false)
+    png_ptr = create_read_struct()
+    info_ptr = create_info_struct(png_ptr)
+    _readcallback_c = @cfunction(_readcallback, C_NULL, (png_structp, png_bytep, png_size_t));
+    png_set_read_fn(pngPtr, s, _readcallback_c)
+    buffer = _load(png_ptr, info_ptr)
+    return formatbuffer(buffer)
+end
+
+function _formatbuffer(buffer)
+    buffer = permutedims(buffer, (2, 1))
+    if expand_paletted || color_type != PNG_COLOR_TYPE_PALETTE
+        return buffer
+    else
+        # We got 0-based indices back from libpng and converting to 1-based could overflow UInt8.
+        # Using UInt16 for index would cost us large part of the savings provided by IndirectArray.
+        return IndirectArray(buffer, OffsetArray(palette, -1))
+    end
+end
+function _readcallback(pngPtr::png_structp, data::png_bytep, length::png_size_t)
+    # Here we get our IO pointer back from the read struct.
+    # This is the parameter we passed to the png_set_read_fn() function.
+    # Our std::istream pointer.
+    a = png_get_io_ptr(pngPtr)
+    # Cast the pointer to std::istream* and read 'length' bytes into 'data'
+    ((std::istream*)a)->read(data, length); #TODO: THIS IS WRONG. Not sure what to do here
+    return C_NULL
+end
+function _load(png_ptr, info_ptr)
     png_set_sig_bytes(png_ptr, PNG_BYTES_TO_CHECK)
     png_read_info(png_ptr, info_ptr)
 
@@ -146,16 +179,8 @@ function load(fpath::String; gamma::Union{Nothing,Float64}=nothing, expand_palet
 
     png_read_image(png_ptr, map(pointer, eachcol(buffer)))
     png_read_end(png_ptr, info_ptr)
-    png_destroy_read_struct(Ref{Ptr{Cvoid}}(png_ptr), Ref{Ptr{Cvoid}}(info_ptr), C_NULL)
-    close_png(fp)
-    buffer = permutedims(buffer, (2, 1))
-    if expand_paletted || color_type != PNG_COLOR_TYPE_PALETTE
-        return buffer
-    else
-        # We got 0-based indices back from libpng and converting to 1-based could overflow UInt8.
-        # Using UInt16 for index would cost us large part of the savings provided by IndirectArray.
-        return IndirectArray(buffer, OffsetArray(palette, -1))
-    end
+    png_destroy_read_struct(Ref{Ptr{Cvoid}}(png_p   tr), Ref{Ptr{Cvoid}}(info_ptr), C_NULL)
+    return buffer
 end
 
 function _buffer_color_type(color_type, bit_depth)
