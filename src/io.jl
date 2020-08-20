66d04cbb379699a6a81c494ee26eb9229552b212
Simon <sdanisch@protonmail.com>
2020-08-20 19:03:29 +0200

    use generic interface instead of accessing fields (#19)
    * use generic interface instead of accessing fields
    
    ....since not all IOs have a lock field...
    
    * dont lock unlockable streams

--- src/io.jl
+++ src/io.jl
@@ -29,20 +29,24 @@ function load(fpath::String; gamma::Union{Nothing,Float64}=nothing, expand_palet
     close_png(fp)
     return out
 end
+
+maybe_lock(f, io::IO) = lock(f, io)
+# IOStream doesn't support locking...
+maybe_lock(f, io::IOStream) = f()
+
 function load(s::IO; gamma::Union{Nothing,Float64}=nothing, expand_paletted::Bool=false)
     isreadable(s) || throw(ArgumentError("read failed, IOStream is not readable"))
     Base.eof(s) && throw(EOFError())
 
     png_ptr = create_read_struct()
     info_ptr = create_info_struct(png_ptr)
-
-    lock(s.lock)
-    png_set_read_fn(png_ptr, s.handle, readcallback_c[])
-    # https://stackoverflow.com/questions/22564718/libpng-error-png-unsigned-integer-out-of-range
-    png_set_sig_bytes(png_ptr, 0)
-    out = _load(png_ptr, info_ptr, gamma=gamma, expand_paletted=expand_paletted)
-    unlock(s.lock)
-    return out
+    
+    maybe_lock(s) do
+        png_set_read_fn(png_ptr, s.handle, readcallback_c[])
+        # https://stackoverflow.com/questions/22564718/libpng-error-png-unsigned-integer-out-of-range
+        png_set_sig_bytes(png_ptr, 0)
+        return _load(png_ptr, info_ptr, gamma=gamma, expand_paletted=expand_paletted)
+    end
 end
 
 function _readcallback(png_ptr::png_structp, data::png_bytep, length::png_size_t)::Cvoid
@@ -290,16 +294,16 @@ function save(
 
     png_ptr = create_write_struct()
     info_ptr = create_info_struct(png_ptr)
-    lock(s.lock)
-    png_set_write_fn(png_ptr, s.handle, writecallback_c[], C_NULL)
+    maybe_lock(s) do
+        png_set_write_fn(png_ptr, s.handle, writecallback_c[], C_NULL)
 
-    _save(png_ptr, info_ptr, image,
-        compression_level=compression_level,
-        compression_strategy=compression_strategy,
-        filters=filters,
-        palette=palette)
+        _save(png_ptr, info_ptr, image,
+            compression_level=compression_level,
+            compression_strategy=compression_strategy,
+            filters=filters,
+            palette=palette)
 
-    unlock(s.lock)
+    end
 end
 
 function _save(png_ptr, info_ptr, image::S;
