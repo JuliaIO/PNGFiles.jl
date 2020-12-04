f3b75910140946464becfffd65b47162be7160a0
contradict <contradict@gmail.com>
2020-12-03 19:56:06 -0800

    Fix test for Base64EncodePipe

--- test/test_io.jl
+++ test/test_io.jl
@@ -17,12 +17,12 @@ test_name, test_image = first(
     end
 end
 
-@testset "Base64EncodedPipe" begin
-    # Waiting on a release which includes "https://github.com/JuliaLang/julia/pull/37520/files"
-    @test_broken test_image == let
+@testset "Base64EncodePipe" begin
+    @test test_image == let
         io = Base64EncodePipe(IOBuffer())
         PNGFiles.save(io, test_image)
-        decoded = IOBuffer(base64decode(take!(io)))
+        close(io)
+        decoded = IOBuffer(base64decode(take!(io.io)))
         PNGFiles.load(decoded)
     end
 end
