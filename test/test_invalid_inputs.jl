7b7f2266c4f0e2042f078a22d6a317930e2efea9
Drvi <tomas.drvostep@gmail.com>
2022-02-04 19:13:05 +0000

    Allow reading background chunk from opaque images

--- test/test_invalid_inputs.jl
+++ test/test_invalid_inputs.jl
@@ -16,8 +16,11 @@ incompatible_background_arguments = [
     ("graya_img_rgb_background", ArgumentError, rand(127, 257, 2), "temp/PngSuite/bgbn4a08.png", RGB{N0f8}(1.0, 0.5, 0.0)),
     ("graya_img_index_background", ArgumentError, rand(127, 257, 2), "temp/PngSuite/bgbn4a08.png", 0x00),
     ("rgba_img_index_background", ArgumentError, rand(127, 257, 4), "temp/PngSuite/bgwn6a08.png", 0x00),
-    ("rgb_no_alpha_background", ArgumentError, rand(127, 257, 3), "temp/PngSuite/basn2c08.png", RGB{N0f8}(0.0)),
-    ("gray_no_alpha_background", ArgumentError, rand(127, 257, 1), "temp/PngSuite/basn0g08.png", Gray{N0f8}(1.0)),
+]
+
+warn_background_arguments = [
+    ("rgb_no_alpha_background", rand(127, 257, 3), "temp/PngSuite/basn2c08.png", RGB{N0f8}(0.0)),
+    ("gray_no_alpha_background", rand(127, 257, 1), "temp/PngSuite/basn0g08.png", Gray{N0f8}(0.0)),
 ]
 
 @testset "Invalid inputs" begin
@@ -52,5 +55,18 @@ incompatible_background_arguments = [
         @testset "palette_img_index_background_not_expandded" begin
             @test_throws ArgumentError PNGFiles.load("test_images/paletted_implicit_transparency.png", background=0x00)
         end
+    end
+
+    warn_msg = "Background color for non-transparent image: PNGFiles._png_color_16_struct(0x00, 0x0000, 0x0000, 0x0000, 0x0000)"
+    for (case, image, load_path, bg) in warn_background_arguments
+        @testset "$(case) throws save" begin
+            @test PNGFiles.save(joinpath(PNG_TEST_PATH, "test_img_err_$(case).png"), image, background=bg) === nothing
+        end
+        @testset "$(case) throws load" begin
+            @test_logs (:warn, warn_msg) PNGFiles.load(load_path, background=bg)
+        end
+    end
+    @testset "non_transparent_image_with_background_warns" begin
+        @test_throws ArgumentError PNGFiles.load("test_images/nontransparent_image_with_background.png", background=0x00)
     end
 end
