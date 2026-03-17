79959602d546532c4e9b5008518e20eddd19fe27
luoxiaofei <luoxiaofei@tongyuan.cc>
2026-03-17 17:38:51 +0800

    update test

--- test/test_win_widechar_filename.jl
+++ test/test_win_widechar_filename.jl
@@ -1,6 +1,14 @@
 @testset "Images with wide char name" begin
-    img16 = PNGFiles.load("test_images/rgb测试16bit.png")
+    src16 = rand(RGB{N0f16}, 50, 50)
+    save("tmp.png", src16)
 
+    tmp16 = PNGFiles.load("tmp.png")
+    mv("tmp.png", "测试rgb16.png", force=true)
+    img16 = PNGFiles.load("测试rgb16.png")
+    rm("测试rgb16.png")
+    
     @test !isempty(img16)
-    @test eltype(img16) === ColorTypes.RGB{FixedPointNumbers.N0f16}
+    @test isequal(src16, tmp16)
+    @test isequal(src16, img16)
+    @test eltype(img16) === ColorTypes.RGB{N0f16}
 end
