280ea6861d50dd72ac259a70f6ee07aef91d24da
luoxiaofei <luoxiaofei@tongyuan.cc>
2026-03-18 09:03:23 +0800

    fix  test errors

--- test/test_win_widechar_filename.jl
+++ test/test_win_widechar_filename.jl
@@ -1,3 +1,5 @@
+using FileIO
+
 @testset "Images with wide char name" begin
     src16 = rand(RGB{N0f16}, 50, 50)
     save("tmp.png", src16)
@@ -6,7 +8,7 @@
     mv("tmp.png", "测试rgb16.png", force=true)
     img16 = PNGFiles.load("测试rgb16.png")
     rm("测试rgb16.png")
-    
+
     @test !isempty(img16)
     @test isequal(src16, tmp16)
     @test isequal(src16, img16)
